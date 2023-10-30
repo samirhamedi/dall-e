18d65974bfcd0435c3d520ea5db1a59d32c8224c
amir <amir.h.hamedi@gmail.com>
2023-10-30 16:20:14 +0330

    localhost changed to server from render

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,12 +21,15 @@ function Home() {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          "https://dall-e-7106.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
         if (response.ok) {
           const result = await response.json();
           setAllPosts(result.data.reverse());
